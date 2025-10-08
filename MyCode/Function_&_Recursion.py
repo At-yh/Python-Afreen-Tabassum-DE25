c7595ba6939f9b04dec757ed874d3e902ccb9c67
Afreen Mohammed <afreenaero@gmail.com>
2025-10-08 11:24:26 +0200

    update file handling

---
+++ MyCode/Function_&_Recursion.py
@@ -0,0 +1,69 @@
+# #            FUNCTIONS in PYTHON
+# # Block of statements that perform a specific task.
+# # Hum apne function ko kuch bhi naam desakte hai like variable me naam dete hai. Aur func jo input leta hai use parameter(val) kehte hai.
+# # functions ko use isliye karte hai ke code k andar se Redundancy(repeat) kam hojai. 
+
+# # a = 2
+# # b = 10
+# # sum = a + b
+# # print(sum)   # samajhne
+
+# def calc_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# # function call me hum argument(val) ko pass karte hai
+# calc_sum(5, 10)
+
+# # more lines code here
+# calc_sum(2, 10)
+
+# # more lines code here
+# calc_sum(12, 17)
+
+# # Function definition
+# def calc_sum(a, b):   # parameters(a & b)
+#     return a + b
+# sum = calc_sum(1, 2)  # (calc_sum) is function call, (1,2) is arguments
+# print(sum)
+
+# # Bina parameter wala func: Na ye koi input lera parameter na ye koi output dera na return val hai.
+# def print_hello():  # its a function
+#     print("hello")
+
+# print_hello()  # its a call
+
+
+# # Calculates the average of 3 numbers
+# def calc_avg(a, b, c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+
+# calc_avg(1, 2, 3)
+
+
+# #      FUNCTIONS IN PYTHON (2 type of functions)
+# #  1.Built-in functions         2.User defined functions
+#    # -like print(), len(), type(), range() are built in python functions.
+#    # -jo functions user(hum) likhte hai use User defined func kehte hai.
+
+
+# #     DEFAULT PARAMETERS
+
+# # To calcukate the multiplication of 2 numbers.
+# def cal_prod(a=2, b=3): # jab bhi hume default val deni hoti hai tho last se dena shuru karte hai
+#     print(a * b)
+#     return a * b
+
+# cal_prod()  
+
+
+
+#    PRACTICE  QUESTIONS
+
+#(Q-1) WAP to print the length of a list. ( list is the parameter)
+
+
